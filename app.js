6198d1ba1bd3852590fab967813d18b2f872853b
Guillermo Sanchez <gsanz1997@gmail.com>
2022-09-15 11:20:42 -0400

    Add feauture to get a random color when moving from cells

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let container = document.getElementById('container');
+const randomColor = document.getElementById('randomColor');
+let rgbColor = null;
+
 createGrid(4);
 
 function createGrid(divisions) {
@@ -9,24 +12,45 @@ function createGrid(divisions) {
         for (let j = 0; j < divisions; j++) {
             let divContainer = document.createElement('div');
             divContainer.classList.add('divContainer');
-            divContainer.addEventListener('mouseover', (event) => {
-                // highlight the mouseenter target
-                event.target.style.backgroundColor = "purple";});
+            divContainer = changeColor(divContainer);
             row.appendChild(divContainer);
         }
         container.appendChild(row);
     }
 }
 
+// Divides grid in NxN entered by user
 let buttonNewGrid = document.getElementById('newGrid');
 buttonNewGrid.addEventListener("click",() => {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
-    let a = Number(document.getElementById('divisions').value);
-    createGrid(a);
+    let divisions = Number(document.getElementById('divisions').value);
+    createGrid(divisions);
+    rgbColor = null;
+
 });
 
-function changeColor(){
-    console.log('hola');
+
+function changeColor(divContainer){
+    divContainer.addEventListener('mouseover', (event) => {
+        let rgb = null;
+        if (rgbColor === null) {
+            rgb = 'purple';
+        } else {
+            getColor();
+            rgb = rgbColor
+        }
+    randomColor.addEventListener("click", getColor);
+    event.target.style.backgroundColor = rgb;});
+    return divContainer;
+}
+
+
+
+function getColor() {
+    let rColor = Math.floor(Math.random() * 256);
+    let gColor = Math.floor(Math.random() * 256);
+    let bColor = Math.floor(Math.random() * 256);
+    rgbColor = `rgb(${rColor} , ${gColor} , ${bColor})`
 }
